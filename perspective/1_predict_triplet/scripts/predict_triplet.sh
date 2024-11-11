e6184333137ebf2e2b5162e01aaa69d3afce0582
Jean-Paul <jpzambrano.leon@gmail.com>
2024-11-11 00:38:46 -0600

    updated file

--- perspective/1_predict_triplet/scripts/predict_triplet.sh
+++ perspective/1_predict_triplet/scripts/predict_triplet.sh
@@ -1,4 +1,4 @@
-#export OPENAI_API_KEY="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"
+
 for dataset in banking77
 do
     link_path=sampled_triplet_results/${dataset}_embed=e5_s=small_m=1024_d=77.0_sf_choice_seed=100.json
